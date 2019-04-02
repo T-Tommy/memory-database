a542743dcee6f3b9daabe618219d2c3f60f5592c
Tommy Tran <tranttommy95@gmail.com>
2019-04-02 16:18:45 -0700

    passing test for create

--- test/index.test.js
+++ test/index.test.js
@@ -9,3 +9,28 @@ describe('Initialize database', () => {
   });
 });
 
+describe('Create', () => {
+  it('creates a copy that contains a uuid and returns the copy', () => {
+    const input = {
+      name: 'Yes',
+      age: 5
+    };
+    const result = new MemoryDatabase();
+    const copy = result.create(input);
+
+    expect(copy.name).toEqual('Yes');
+    expect(copy.age).toEqual(5);
+    expect.any(copy._id);
+  });
+
+  it('store the copy into the store', () => {
+    const input = {
+      name: 'Yes',
+      age: 5
+    };
+    const result = new MemoryDatabase();
+    const copy = result.create(input);
+  
+    expect(result.store[copy._id]).toEqual(copy);
+  });
+});
